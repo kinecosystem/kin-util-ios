34259329886f618c32993eb74bd7f41f5d4a764a
ashevin <avi.shevin@kik.com>
2018-02-25 17:23:17 +0200

    Add Promise and Observable.

---
+++ KinUtil/KinUtil/KinUtil.h
@@ -0,0 +1,19 @@
+//
+//  KinUtil.h
+//  KinUtil
+//
+//  Created by Avi Shevin on 25/02/2018.
+//  Copyright © 2018 Kin Foundation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KinUtil.
+FOUNDATION_EXPORT double KinUtilVersionNumber;
+
+//! Project version string for KinUtil.
+FOUNDATION_EXPORT const unsigned char KinUtilVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KinUtil/PublicHeader.h>
+
+
